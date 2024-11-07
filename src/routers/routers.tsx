3520455f941eb8650a256f787b909ed7453988b1
bentenismyidol <nguyenvietthanh18112002@gmail.com>
2024-11-07 23:39:33 +0700

    Redesign Sidebar, need to redesign Navbar and table, Router project structure as well

---
+++ src/routers/routers.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import { Login } from "../views/Login";
+import { ChangePassword } from "../views/ChangePassword";
+import { ProtectedRouters } from "./ProtectedRouter.tsx";
+import { Dashboard } from "../views/Dashboard";
+
+export const routers = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "",
+        element: <div>Home</div>,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/signup",
+        element: <div>Signup</div>,
+      },
+      {
+        path: "/dashboard",
+        element: (
+          <ProtectedRouters>
+            <Dashboard />
+          </ProtectedRouters>
+        ),
+      },
+      {
+        path: "/change-password",
+        element: <ChangePassword />,
+      },
+    ],
+  },
+]);
