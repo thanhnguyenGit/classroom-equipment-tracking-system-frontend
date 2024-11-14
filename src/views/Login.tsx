cd110727e9aedca285bf2df868e659d7d8817a00
Nguyen Thanh Cong <thanhcong3112002@gmail.com>
2024-11-14 10:47:47 +0700

    Update login form

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -3,20 +3,69 @@ import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/useAuth";
 import { AppProvider } from '@toolpad/core/AppProvider';
 import { SignInPage, type AuthProvider } from '@toolpad/core/SignInPage';
-import { useTheme } from '@mui/material/styles';
+import {
+  Avatar,
+  Box,
+  Container,
+  FormControlLabel,
+  Paper,
+  TextField,
+  Typography,
+  Checkbox,
+  Button,
+  Grid,
+  Link,
+} from "@mui/material";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 
 export const Login = () => {
   const { login } = useAuth();
   const naviagte = useNavigate();
   return (
-    <div>
-      <div>
-        <p>User name</p>
-        <input type="text" />
-        <p>Password</p>
-        <input type="text" />
-      </div>
-      <button onClick={login}>Login</button>
-    </div>
+    // <div className="Login-container">
+    //   <div className="title">Login</div>
+    //   <input />
+    //   <input />
+    //   <button onClick={login}>Login</button>
+    // </div>
+    <Container maxWidth="xs">
+      <Paper elevation={10} sx={{ marginTop: 8, padding: 2 }}>
+        <Avatar
+          sx={{
+            mx: "auto",
+            bgcolor: "secondary.main",
+            textAlign: "center",
+            mb: 1,
+          }}
+        >
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5" sx={{ textAlign: "center" }}>
+          Sign In
+        </Typography>
+        <Box component="form" noValidate sx={{ mt: 1 }}>
+          <TextField
+            placeholder="Enter username"
+            fullWidth
+            required
+            autoFocus
+            sx={{ mb: 2 }}
+          />
+          <TextField
+            placeholder="Enter password"
+            fullWidth
+            required
+            type="password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button type="submit" variant="contained" fullWidth sx={{ mt: 1 }}>
+            Sign In
+          </Button>
+        </Box>
+      </Paper>
+    </Container>
   );
 };
